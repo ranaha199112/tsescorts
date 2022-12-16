cd1bf41e94ce178923fad61bd6a7c5d25763b859
Ashfak Niloy <ashfakniloy56@gmail.com>
2022-12-16 22:18:06 +0600

    changed site name

--- config/index.js
+++ config/index.js
@@ -1,3 +1,4 @@
 export const API_URL = "https://phisback.vercel.app";
 
-export const site = "tsescorts.vercel.app";
+export const site = "www.tsescort.live";
+// export const site = "tsescorts.vercel.app";
