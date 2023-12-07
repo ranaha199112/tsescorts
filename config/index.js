c9279b03f18427d4678bd18f3bd48d205b8511a1
ranaha199112 <ranaha199112@gmail.com>
2023-12-07 12:44:54 +0600

    changed domain

--- config/index.js
+++ config/index.js
@@ -1,4 +1,5 @@
 export const API_URL = "https://phisback.vercel.app";
 
-export const site = "www.tsescort.live";
+export const site = "www.tsescortz.online";
+// export const site = "www.tsescort.live";
 // export const site = "tsescorts.vercel.app";
