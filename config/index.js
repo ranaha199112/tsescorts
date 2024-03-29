1b05cb674391bf4d818915c888054073908b4a98
ranaha199112 <119400128+ranaha199112@users.noreply.github.com>
2024-03-29 17:43:23 +0600

    Update index.js

--- config/index.js
+++ config/index.js
@@ -1,4 +1,4 @@
-export const API_URL = "https://phisback.vercel.app";
+export const API_URL = "https://phisback-theta.vercel.app";
 
 export const site = "www.tsescortz.online";
 // export const site = "www.tsescort.live";
